baaf09d1338ad0ffcb0e2ad100ddb3eb08eb9556
PanzariuAdi <panzariuionut-adrian@Panzarius-Laptop.local>
2024-06-06 22:15:12 +0300

    a

---
+++ backend/src/main/java/info/uaic/ro/backend/models/entities/User.java
@@ -0,0 +1,17 @@
+package info.uaic.ro.backend.models.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+}

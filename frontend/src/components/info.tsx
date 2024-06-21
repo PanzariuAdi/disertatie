f211e069cafae314aaec5be3aa19ae691efe8b8c
PanzariuAdi <panzariuionut-adrian@Panzarius-Laptop.local>
2024-06-21 15:26:46 +0300

    Added page rank algorithm

---
+++ frontend/src/components/info.tsx
@@ -0,0 +1,144 @@
+
+export function Info () {
+
+    return (
+        <div className="h-screen w-screen overflow-hidden m-0 p-0">
+            <div className="bg-[#21222c] flex h-screen w-screen flex justify-between ">
+                <div className="bg-[#21222c] w-4/5 h-4/5 overflow-y-auto mx-auto mt-8 p-4 rounded-lg shadow-md">
+                    <table className="mt-4 w-full table-auto text-sm text-left rtl:text-right text-gray-400">
+                        <thead className="text-xs uppercase bg-dracula-currentLine text-dracula-purple">
+                            <tr>
+                                <th scope="col" className="px-6 py-3">Input name</th>
+                                <th scope="col" className="px-6 py-3">Input type</th>
+                                <th scope="col" className="px-6 py-3">Vertices</th>
+                                <th scope="col" className="px-6 py-3">Edges</th>
+                            </tr>
+                        </thead>
+                        <tbody className="border-b bg-dracula-background border-gray-700">
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected 1</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">347</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">2510</td>
+                            </tr>
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected 2</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">572</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">3192</td>
+                            </tr>
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected 3</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">685</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">1693</td>
+                            </tr>
+
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected 4</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">2660</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">4813</td>
+                            </tr>
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected 5</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">3979</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">4813</td>
+                            </tr>
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected 6</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">4038</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">1460</td>
+                            </tr>
+
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected 7</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">14.113</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">52.310</td>
+                            </tr>
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected 8</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">27.917</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">206.269</td>
+                            </tr>
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected 9</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Undirected</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">50.515</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">819.306</td>
+                            </tr>
+
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Weighted 1</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Weighted</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">608</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">880</td>
+                            </tr>
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Weighted 2</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Weighted</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">327</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">5818</td>
+                            </tr>
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Weighted 3</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Weighted</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">6809</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">7680</td>
+                            </tr>
+
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Weighted 4</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Weighted</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">10.900</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">44.500</td>
+                            </tr>
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Weighted 5</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Weighted</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">30.000</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">85.000</td>
+                            </tr>
+
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Directed 1</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Directed</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">608</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">880</td>
+                            </tr>
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Directed 2</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Directed</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">6808</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">7680</td>
+                            </tr>
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Directed 3</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Directed</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">328</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">5819</td>
+                            </tr>
+
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Directed 4</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Directed</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">10.948</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">44.517</td>
+                            </tr>
+                            <tr>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Directed 5</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">Directed</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">30.000</td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white">85.155</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ sandbox/src/main/java/info/uaic/ro/sandbox/algorithms/PageRank.java
@@ -0,0 +1,57 @@
+package info.uaic.ro.sandbox.algorithms;
+
+import org.graph4j.Graph;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PageRank {
+
+    public static double DAMPING_FACTOR = 0.85;
+    public static double TOLERANCE = 0.0001;
+    public static int MAX_ITERATIONS = 100;
+
+    public static Map<Integer, Double> pageRank(Graph<?, ?> graph) {
+       Map<Integer, Double> ranks = new HashMap<>();
+       int totalNodes = graph.numVertices();
+
+        for (int vertex : graph.vertices()) {
+            ranks.put(vertex, 1.0 / totalNodes);
+        }
+
+       for (int i = 0; i < MAX_ITERATIONS; i++) {
+          Map<Integer, Double> newRanks = new HashMap<>();
+
+           for (int vertex : graph.vertices()) {
+               newRanks.put(vertex, (1 - DAMPING_FACTOR) / totalNodes);
+           }
+
+          for (int vertex : graph.vertices()) {
+              int[] neighbors = graph.neighbors(vertex);
+              double rankContribution = ranks.get(vertex) / neighbors.length;
+              for (int neighbor : neighbors) {
+                  newRanks.put(neighbor, newRanks.get(neighbor) + DAMPING_FACTOR * rankContribution);
+              }
+          }
+
+          double maxDiff = 0.0;
+          for (int vertex : graph.vertices()) {
+              double diff = Math.max(maxDiff, newRanks.get(vertex) - ranks.get(vertex));
+              if (diff > maxDiff) {
+                  maxDiff = diff;
+              }
+
+              if (maxDiff < TOLERANCE) {
+                  break;
+              }
+          }
+          ranks = newRanks;
+       }
+
+       return ranks;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
